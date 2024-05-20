6c53e05ffd39c707169f4f750ecb95451d0aacd0
maxist2 <59034778+maxist2@users.noreply.github.com>
2024-05-20 17:46:01 -0500

    modo obscuro y claro 20/5/24

--- font/scripts/cambiocolo.js
+++ font/scripts/cambiocolo.js
@@ -1,22 +1,25 @@
-const originalColors = { 
-    '--color-1': 'rgb(255, 87, 34)',
-    '--color-15': '255, 87, 34',
-    '--color-2': 'rgb(255, 235, 59)',
-    '--color-3': 'rgb(76, 175, 80)',
-    '--color-4': 'rgb(33, 33, 33)'
-    
+const originalColors = {
+    '--color-1': 'rgb(34, 40, 49)',   /* color oscuro */
+    '--color-2': 'rgb(238, 238, 238)', /* color claro */
+    '--color-3': 'rgb(118, 171, 174)', /* color intermedio */
+    '--color-4': 'rgb(49, 54, 63)',    /* otro color oscuro */
+    '--color-5': 'rgb(0, 0, 0)',       /* negro */
+    '--color-6': 'rgba(128, 128, 128, 0.489)' /* color semi-transparente */
 };
 
 const newColors = {
-    '--color-1': 'rgb(34, 40, 49)',
-    '--color-15': '34, 40, 49',
-    '--color-2': 'rgb(238, 238, 238)',
-    '--color-3': 'rgb(118, 171, 174)',
-    '--color-4': 'rgb(49, 54, 63)'
-   
+    '--color-1': 'rgb(238, 238, 238)',   /* color claro */
+    '--color-2': 'rgb(34, 40, 49)',      /* color oscuro */
+    '--color-3': 'rgb(63, 86, 152)',     /* color intermedio */
+    '--color-4': 'rgb(156, 163, 176)',   /* otro color intermedio */
+    '--color-5': 'rgb(34, 40, 49)',      /* otro color oscuro derivado de --color-15 */
+    '--color-6': 'rgba(128, 128, 128, 0.489)' /* color semi-transparente */
 };
 
 let usingOriginalColors = true;
+
+const changeColorsButton = document.getElementById('change-colors');
+const changeColorsButtonIcon = changeColorsButton.querySelector('img');
 
 document.getElementById('change-colors').addEventListener('click', function() {
     const colors = usingOriginalColors ? newColors : originalColors;
@@ -24,4 +27,6 @@ document.getElementById('change-colors').addEventListener('click', function() {
         document.documentElement.style.setProperty(key, value);
     }
     usingOriginalColors = !usingOriginalColors;
+    // Cambiar el icono del botón
+    changeColorsButtonIcon.src = usingOriginalColors ? 'recursos/claro.png' : 'recursos/obscuro.png';
 });
